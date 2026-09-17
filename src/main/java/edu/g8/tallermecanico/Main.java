52714631a3fec19743cfada31e27b3fe0db66446
Jorchito-SDS <jorgemejia4899@gmail.com>
2026-09-17 10:09:01 -0600

    fix: correción directa en main

--- src/main/java/edu/g8/tallermecanico/Main.java
+++ src/main/java/edu/g8/tallermecanico/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package edu.g8.tallermecanico;
+package main.java.edu.g8.tallermecanico;
 
 /**
  *
