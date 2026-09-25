ec1515abc1e1773312f084d88ee70a7ef68636de
Jorchito-SDS <jorgemejia4899@gmail.com>
2026-09-24 23:41:27 -0600

    fix: correcion de controllers

---
+++ src/main/java/edu/g8/tallermecanico/controller/GestionClienteController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.edu.g8.tallermecanico.controller;
+
+/**
+ *
+ * @author 15-DY1005LA
+ */
+public class GestionClienteController {
+    
+}
